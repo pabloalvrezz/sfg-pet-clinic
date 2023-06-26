47835a5392d2227ccc40b071afe9b3d2d2c3c4b8
Pablo Alvarez Gonzalez <pagonzalez@izertis.com>
2023-06-26 08:58:21 +0200

    Version inicial de los modelos

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+	private PetType petType;
+	private Owner owner;
+	private LocalDate birthDate;
+}
