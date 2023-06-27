867804eaf280c3fb748ac0addfaa85c22cc61082
Pablo Alvarez Gonzalez <pagonzalez@izertis.com>
2023-06-27 16:22:23 +0200

    Añadimos la lista de Vets

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.sfgpetclinic.services.VetService;
+@RequestMapping("/vets")
 @Controller
 public class VetController {
-	
-	@RequestMapping({"/vets","/vets/index","vets/index.html"})
-	public String index() {
+
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({ "/", "/index", "/index.html" })
+	public String index(Model model) {
+		model.addAttribute("vets", vetService.findAll());
+		
 		return "vets/index";
 	}
 }
