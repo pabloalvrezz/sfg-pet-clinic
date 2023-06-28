45525c6d54a6716df740e52fcf09f6186a004cf5
Pablo Alvarez Gonzalez <pagonzalez@izertis.com>
2023-06-28 12:11:39 +0200

    Añadimos la entidad de BaseEntity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -2,8 +2,17 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable{
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
