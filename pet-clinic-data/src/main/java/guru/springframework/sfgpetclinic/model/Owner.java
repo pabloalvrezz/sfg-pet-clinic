af9f57b927cb9909cd4a9398d1210792035939b4
Pablo Alvarez Gonzalez <pagonzalez@izertis.com>
2023-06-28 12:19:28 +0200

    Convertimos los owners en JPA identities

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -3,11 +3,27 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Owners")
 public class Owner extends Person {
 
+	@Column(name = "addres")
 	private String address;
+	
+	@Column(name = "city")
 	private String city;
+	
+	@Column(name = "telephone")
 	private String telephone;
+	
+	// si se elimina un owner se elimina las mascotas que tiene este
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
 	private Set<Pet> pets = new HashSet<>();
 	
 	public String getAddress() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,11 +2,28 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Pet")
 public class Pet extends BaseEntity{
 
+	@Column(name = "name")
 	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "type_id")
 	private PetType petType;
+	
+	@ManyToOne
+	@JoinColumn(name = "owner_id")
 	private Owner owner;
+	
+	@Column(name = "bith_Date")
 	private LocalDate birthDate;
 	
 	public String getName() {
