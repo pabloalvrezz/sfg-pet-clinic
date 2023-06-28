2789f40925f189d3334aef042e7ffa7c7cd0b0b8
Pablo Alvarez Gonzalez <pagonzalez@izertis.com>
2023-06-28 10:17:10 +0200

    Añadimos los pet Type al data Loader

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Component
@@ -13,10 +15,12 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
@@ -49,6 +53,18 @@ public class DataLoader implements CommandLineRunner {
 		vetService.save(vet2);
 
 		System.out.println("Vets cargados......");
+		
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		
+		petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		cat.setName("cat");
+		
+		petTypeService.save(cat);
+		
+		System.out.println("Pet Types cargados");
 
 	}
 
